cd26601ea167701a38dcf09396b58129103a7487
Vishal Garg <vishugarg10004@gmail.com>
2020-10-20 11:54:37 +0530

    [Vishal] added code to find max from float data type

--- FindMaxProblem/Program.cs
+++ FindMaxProblem/Program.cs
@@ -4,13 +4,22 @@ namespace FindMaxProblem
 {
     class Program
     {
+        /// <summary>
+        /// Implementing different methods from different classes
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to find max problem");
+            //used to catch exception
             try
             {
+                //Calling non generic method to find max of 3 integers
                 int maxInt= NonGenericClass.MaxIntegerNumberAmongThree(23, 24, 22);
                 Console.WriteLine("max integer value is\t" + maxInt);
+                //calling non generic method to find max of 3 floats
+                float maxFloat = NonGenericClass.MaxfloatNumberAmongThree(23, 24, 22);
+                Console.WriteLine("max float value is\t" + maxFloat);
             }
             catch(Exception ex)
             {
--- findMaxMSTest/UnitTest1.cs
+++ findMaxMSTest/UnitTest1.cs
@@ -6,6 +6,13 @@ namespace findMaxMSTest
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Used to test MaxIntegers numbers among three from non generic class
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="expected"></param>
         
         [TestMethod]
         [DataRow(45, 47, 60, 60)]
@@ -16,6 +23,25 @@ namespace findMaxMSTest
             //Arrange
             //add
             int actual= NonGenericClass.MaxIntegerNumberAmongThree(a, b, c);
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        /// <summary>
+        /// used to test max float no from three coming from non generic class
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="expected"></param>
+        [TestMethod]
+        [DataRow(45.1f, 47.2f, 60.3f, 60.3f)]
+        [DataRow(45.6f, 122.4f, 60.5f, 122.4f)]
+        [DataRow(440.7f, 47.8f, 60.9f, 440.7f)]
+        public void MaxFromThreeFloats(float a, float b,float c, float expected)
+        {
+            //Arrange
+            //add
+            float actual = NonGenericClass.MaxfloatNumberAmongThree(a, b, c);
             //Assert
             Assert.AreEqual(actual, expected);
         }
